6b901f7dafb7a9e2a8fa736bc46df40ed5e129ef
styrofoammilk <matthew.geier.mg@gmail.com>
2023-07-23 16:58:41 -0400

    requirements file

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import pandas as pd
 from german_flash_card_def_function import generate_flash_cards
 import spacy
 
+# Download and install the German language model for spaCy if not already installed
+try:
+    nlp = spacy.load('de_core_news_md')
+except OSError:
+    import subprocess
+    subprocess.run(['python', '-m', 'spacy', 'download', 'de_core_news_md'])
+    nlp = spacy.load('de_core_news_md')
+
 def app():
     text_input = st.text_area("Paste your text here:")
     if st.button("Get your German flash cards"):
