d64d3697dd6d109ba890bd51c385618ceb3e4d2e
styrofoammilk <matthew.geier.mg@gmail.com>
2023-07-23 17:02:15 -0400

    requirements file

--- app.py
+++ app.py
@@ -11,20 +11,6 @@ except OSError:
     subprocess.run(['python', '-m', 'spacy', 'download', 'de_core_news_md'])
     nlp = spacy.load('de_core_news_md')
 
-def app():
-    text_input = st.text_area("Paste your text here:")
-    if st.button("Get your German flash cards"):
-        flash_cards = generate_flash_cards(text_input)
-
-        # Print flash cards
-        st.markdown("Nouns:")
-        st.markdown(flash_cards["NOUN"])
-        st.markdown("Verbs:")
-        st.markdown(flash_cards["VERB"])
-        st.markdown("Adjectives:")
-        st.markdown(flash_cards["ADJ"])
-
-        # ... and so on
 def main():
     st.title('Get Flash Cards')
     user_input = st.text_input('Enter your text here:')
